bdc162b8cbfa7c0cec62ca4da8fac0295467cbfd
Alina Bychkova <ialina240200@gmail.com>
2021-04-01 01:06:26 +0300

    Finish header styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -52,21 +52,24 @@ const Header = (props) => {
             {showUserMenu &&
               <div className={s.user_menu_wrapper}>
                 <div className={s.user_menu}>
-                  <div className={s.header}>
-                    <div className={s.avatar_wrapper}>
-                      <img src={"//localhost:3000" + user.avatar.thumb.url} alt="" />
+                  <NavLink to='/profile'>
+                    <div className={s.header}>
+                      <div className={s.avatar_wrapper}>
+                        <img src={"//localhost:3000" + user.avatar.thumb.url} alt="" />
+                      </div>
+                      <div className={s.bio}>
+                        <div className={s.name}>{user.user_name}</div>
+                        <div className={s.login}>@{user.login}</div>
+                      </div>
                     </div>
-                    <div className={s.bio}>
-                      <div className={s.name}>{user.user_name}</div>
-                      <div className={s.login}>@{user.login}</div>
-                    </div>
-                  </div>
+                  </NavLink>
+                  <div className={s.separator}></div>
                   <div className={s.links}>
-                    <NavLink to='/profile' className={s.link}>профиль</NavLink>
                     <NavLink to='/profile' className={s.link}>мой бар</NavLink>
                     <NavLink to='/profile' className={s.link}>избранное</NavLink>
                     <NavLink to='/profile' className={s.link}>настройки</NavLink>
                   </div>
+                  <div className={s.separator}></div>
                   <div className={s.logout} onClick={handleLogout} >
                     выйти
                   </div>
