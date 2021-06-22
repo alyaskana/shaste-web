097d0ff52a3be174810e09c92dfcb8615480a29e
Alina Bychkova <ialina240200@gmail.com>
2021-06-22 13:44:15 +0300

    Add footer

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,9 +1,19 @@
+import { Link } from 'react-router-dom'
 import s from './Footer.module.scss'
+import { ReactComponent as SignSvg } from '@/assets/images/sign_black.svg'
 
 export const Footer = () => {
   return (
     <div className={s.footer}>
-      <div>Footer</div>
+      <div className={s.footer_content}>
+        <div>©2020 – 2021, Shast’e</div>
+        <SignSvg className={s.logo} />
+        <div className={s.social_links}>
+          <Link to="/">youtube</Link>
+          <Link to="/">facebook</Link>
+          <Link to="/">instagram</Link>
+        </div>
+      </div>
     </div>
   )
 }
