7b257719036c359e57e0c151ed3995e12a459f39
Alina Bychkova <ialina240200@gmail.com>
2021-06-26 03:56:43 +0300

    Add feed page

--- src/api/posts/index.ts
+++ src/api/posts/index.ts
@@ -2,7 +2,15 @@ import { API } from '../base'
 import { Post } from '../../types'
 import { serialize } from 'object-to-formdata'
 
+type PostsResponse = {
+  posts: Post[]
+}
+
 class PostsFetcher extends API {
+  getAll<R = PostsResponse>() {
+    return super.get<R>({ path: 'posts' })
+  }
+
   create<R = Post>(data: Record<string, unknown>) {
     return super.post<R>({
       path: `posts`,
--- src/pages/feed/FeedPage.tsx
+++ src/pages/feed/FeedPage.tsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from 'react'
+import { Post as PostType } from '../../types'
+import { postsFetcher } from '../../api/posts'
+
+import s from './FeedPage.module.scss'
+import { Post } from '@components/common/Post'
+
 const FeedPage = () => {
+  const [posts, setPosts] = useState<PostType[]>([])
+
+  useEffect(() => {
+    postsFetcher.getAll().then((response) => {
+      setPosts(response.data.posts)
+    })
+  }, [])
+
   return (
-    <div>
-      <h1>Feed page will be here</h1>
+    <div className={s.feed_page}>
+      {posts.map((post) => (
+        <div key={post.id} className={s.post_item}>
+          <Post post={post} isRecipeCardShowing={true} />
+        </div>
+      ))}
     </div>
   )
 }
